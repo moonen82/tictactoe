03c1c1e1f09722d1ac9052c5d6c36e7bcc5100a2
Michiel Moonen <mgmmoonen@gmail.com>
2023-09-04 22:17:13 +0200

    added board win conditions, 3 methods

--- tictactoe.py
+++ tictactoe.py
@@ -7,7 +7,9 @@ board = [ "1", "2", "3",
 # build grid
 ''' example grid
 X | X | X
+----------
 O | O | O
+----------
 X | O | X
 '''
 
@@ -38,6 +40,38 @@ class GameBoard:
         for i in range(9):
             self.board[i] = ' '
 
+    def check_row_win(self):
+        if self.board[0] == self.board[1] == self.board[2] and self.board[0] != " ":
+            self.winner = self.board[0]
+            return True
+        if self.board[3] == self.board[4] == self.board[5] and self.board[3] != " ":
+            self.winner = self.board[3]
+            return True
+        if self.board[6] == self.board[7] == self.board[8] and self.board[6] != " ":
+            self.winner = self.board[6]
+            return True
+
+    def check_col_win(self):
+        if self.board[0] == self.board[3] == self.board[6] and self.board[0] != " ":
+            self.winner = self.board[0]
+            return True
+        if self.board[1] == self.board[4] == self.board[7] and self.board[1] != " ":
+            self.winner = self.board[1]
+            return True
+        if self.board[2] == self.board[5] == self.board[8] and self.board[2] != " ":
+            self.winner = self.board[2]
+            return True
+
+    def check_diag_win(self):
+        if self.board[0] == self.board[4] == self.board[8] and self.board[0] != " ":
+            self.winner = self.board[0]
+            return True
+        if self.board[2] == self.board[4] == self.board[6] and self.board[2] != " ":
+            self.winner = self.board[2]
+            return True
+
+    
+
 # define player X, ask name
 
 # define player O, ask name
@@ -46,7 +80,7 @@ class GameBoard:
 
 # start game
 
-# first player, ask place in grid to start
+
 
 # second player, ask place in grid to place
 
@@ -56,16 +90,18 @@ class GameBoard:
 
 # play again?
 
-# TEST GRID INPUT
 
+# first player, ask place in grid
 print('Player X - provide the spot you want to mark: ')
 coordX = int(input())
+# check if input is valid
 while (coordX < 1 or coordX > 9):
     print(f'{coordX} is invalid, choose a number from 1 to 9 : ')
     coordX = int(input())
 #print('Player Y - provide the spot you want to mark: ')
 #coordY= input()
 
+# TEST GRID INPUT
 game_board1 = GameBoard(board)
 
 
